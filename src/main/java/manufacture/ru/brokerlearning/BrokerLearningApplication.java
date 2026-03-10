ca41979f80ccbed18271b93144e3555bdb78ac83
ShepelevIS <isshepelev2004@gmail.com>
2026-03-10 13:09:55 +0300

    first commit

---
+++ src/main/java/manufacture/ru/brokerlearning/BrokerLearningApplication.java
@@ -0,0 +1,13 @@
+package manufacture.ru.brokerlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokerLearningApplication.class, args);
+    }
+
+}
